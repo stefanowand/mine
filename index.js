04b9471ab9074ef2e4a57d6c8db02e366ed6493e
stefanowand <37331142+stefanowand@users.noreply.github.com>
2018-09-06 21:09:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('k933H95YHa7kopyrXwx5ljEPWkLccS5n'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
